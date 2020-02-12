6dde10c8e8f16dc5c3f75383b65185bb86dd0108
Ivan Kovalets <kovaletsivan@gmail.com>
2020-02-12 21:50:35 +0200

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Task 1
+
+$("a[href^='https://']").attr("target", "_blank");
+
+// Task 2
+
+$el = $("h2.head*").css("background", "green");
+$($el)
+  .find(".inner")
+  .css("fontSize", "35px");
+
+// Task 3
+
+$div = $("h3+div:first");
+$("h3:first").before($div);
+$div = $("h3+div:last");
+$("h3:last").before($div);
+
+// Task 4
+
+$("input").on("click", function() {
+  $("input").each(function() {
+    if ($(this).prop("checked") == !true) {
+      $(this).prop("disabled", "true");
+    }
+  });
+});
